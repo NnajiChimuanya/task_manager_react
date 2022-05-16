45c7d8651ceef54b711fe72f32054eb62d078323
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-16 13:42:31 +0400

    Made some changes

--- src/components/List.js
+++ src/components/List.js
@@ -7,10 +7,10 @@ export default function List({items, editItem, deleteItem }) {
     {items.map((item) => {
         const {id, name} = item
         return(
-          <section key={id}>
+          <section className='section' key={id}>
               <p className='itemName'> {name} </p>
-              <button onClick={() => editItem(id)}> <AiFillEdit /> </button>
-              <button onClick={() => deleteItem(id)}> <AiFillDelete /> </button>
+              <button className='edit-btn' onClick={() => editItem(id)}> <AiFillEdit /> </button>
+              <button className='delete-btn' onClick={() => deleteItem(id)}> <AiFillDelete /> </button>
           </section>
         )
     })}
