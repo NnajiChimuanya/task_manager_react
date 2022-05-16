4fbf8608f0b4224ed729444d144653ba25b8b3d3
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-16 12:12:54 +0400

    Added the edit, delete, submit functions

--- src/App.js
+++ src/App.js
@@ -5,12 +5,45 @@ import List from './components/List';
 function App() {
   const [name, setName] = useState("")
   const [list, setList] = useState([])
+  const [ isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    setList([...list, name])
+    if(!name) {
+      //will set up later
+    } else if(name && isEditing) {
+      setList(list.map(item =>{
+        if(item.id === editId) {
+          return {...item, name : name}
+        }
+        return item
+      }))
+      setName("")
+      setEditId(null)
+      setIsEditing(false)
+    } else {
+      let newItem = { id: new Date().getMilliseconds(), name : name}
+      setList([...list, newItem])
+      setName("")
+    }
+
+  }
+
+  const editItem = (id) => {
+    
+    let theItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditId(theItem.id)
+    setName(theItem.name)
+
+  }
+
+  const deleteItem = (id) => {
+    let newItems = list.filter(item => item.id !== id)
+    setList(newItems)
   }
 
  return (
@@ -18,10 +51,10 @@ function App() {
      <form onSubmit={handleSubmit}>
        <h2> Task manager </h2>
        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-       <button type="submit"> Add Task</button>
+       <button type="submit"> {isEditing ? "Edit Task" : "Add Task"} </button>
       </form>
       <div>
-        { list && <List items={list} />}
+        { list && <List items={list} editItem={editItem} deleteItem={deleteItem} />}
       </div>
    </div>
  )
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
-export default function List( ) {
+export default function List({items, editItem, deleteItem }) {
   return (
     <>
-    
+    {items.map((item) => {
+        const {id, name} = item
+        return(
+          <article key={id}>
+              <h3> {name} </h3>
+              <button onClick={() => editItem(id)}> <AiFillEdit /> </button>
+              <button onClick={() => deleteItem(id)}> <AiFillDelete /> </button>
+          </article>
+        )
+    })}
     </>
   );
 }
